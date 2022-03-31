39f0a5561d4175038dc0098f8b23f4c38fc6713e
Djole-zr <93897601+Djole-zr@users.noreply.github.com>
2022-04-01 00:01:49 +0200

    edit list

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 
 const mongoose = require('mongoose');
 const List = require('./models/list')
@@ -12,6 +14,9 @@ async function main() {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
+
 
 app.get('/lists', async (req, res) => {
     const lists = await List.find({})
@@ -22,10 +27,28 @@ app.get('/lists/new', (req, res) => {
     res.render('newList')
 })
 
+app.post('/lists', async (req, res) => {
+    const newList = new List(req.body);
+    await newList.save();
+    res.redirect('/lists')
+})
+
 app.get('/lists/:id', async (req, res) => {
     const { id } = req.params;
     const list = await List.findById(id);
     res.render('show', { list })
+})
+
+app.get('/lists/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const list = await List.findById(id);
+    res.render('edit', { list })
+})
+
+app.put('/lists/:id', async (req, res) => {
+    const { id } = req.params;
+    const list = await List.findByIdAndUpdate(id, req.body, { runValidators: true});
+    res.redirect(`/lists/${id}`);
 })
 
 app.listen(3000, () => {
